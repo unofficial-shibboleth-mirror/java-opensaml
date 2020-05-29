82ac1d8b087458beb3327ae00efa454d70226271
Brent Putman <putmanb@georgetown.edu>
2020-05-28 20:22:30 -0400

    OSJ-311: Migrate tests accessing shibboleth.net to test.shibboleth.net

--- opensaml-saml-impl/src/test/java/org/opensaml/saml/metadata/resolver/impl/FunctionDrivenDynamicHTTPMetadataResolverTest.java
+++ opensaml-saml-impl/src/test/java/org/opensaml/saml/metadata/resolver/impl/FunctionDrivenDynamicHTTPMetadataResolverTest.java
@@ -171,7 +171,7 @@ public class FunctionDrivenDynamicHTTPMetadataResolverTest extends XMLObjectBase
     @Test
     public void testTemplateNonexistentPath() throws Exception {
         // Bad path, resulting in 404.  Should silently fail.
-        String template = "http://shibboleth.net/unittests/metadata?entityID=${entityID}";
+        String template = "http://test.shibboleth.net/unittests/metadata?entityID=${entityID}";
         String entityID = "https://www.example.org/sp";
         
         TemplateRequestURLBuilder requestURLBuilder = new TemplateRequestURLBuilder(
@@ -215,7 +215,7 @@ public class FunctionDrivenDynamicHTTPMetadataResolverTest extends XMLObjectBase
     
     @Test
     public void testMDQ() throws Exception {
-        String baseURL = "http://shibboleth.net:9000";
+        String baseURL = "http://test.shibboleth.net:9000";
         String entityID = "https://foo1.example.org/idp/shibboleth";
         
         MetadataQueryProtocolRequestURLBuilder requestURLBuilder = new MetadataQueryProtocolRequestURLBuilder(baseURL);
@@ -236,7 +236,7 @@ public class FunctionDrivenDynamicHTTPMetadataResolverTest extends XMLObjectBase
     
     @Test
     public void testMDQViaArtifact() throws Exception {
-        String baseURL = "http://shibboleth.net:9000";
+        String baseURL = "http://test.shibboleth.net:9000";
         String entityID = "https://foo1.example.org/idp/shibboleth";
         
         MetadataQueryProtocolRequestURLBuilder requestURLBuilder = new MetadataQueryProtocolRequestURLBuilder(baseURL,
