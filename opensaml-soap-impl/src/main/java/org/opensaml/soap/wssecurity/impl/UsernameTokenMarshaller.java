c1ad3302a7dfcdb993b71c5b5eaeb5d29d7cc90c
Rod Widdowson <rdw@steadingsoftware.com>
2022-06-13 20:22:09 +0100

    Checkstyle

--- opensaml-soap-impl/src/main/java/org/opensaml/soap/wssecurity/impl/UsernameTokenMarshaller.java
+++ opensaml-soap-impl/src/main/java/org/opensaml/soap/wssecurity/impl/UsernameTokenMarshaller.java
@@ -44,7 +44,8 @@ public class UsernameTokenMarshaller extends AbstractWSSecurityObjectMarshaller 
     }
     
     /** {@inheritDoc} */
-    protected void marshallAttributeIDness(XMLObject xmlObject, Element domElement) throws MarshallingException {
+    protected void marshallAttributeIDness(final XMLObject xmlObject,
+            final Element domElement) throws MarshallingException {
         XMLObjectSupport.marshallAttributeIDness(UsernameToken.WSU_ID_ATTR_NAME, domElement, true);
         
         super.marshallAttributeIDness(xmlObject, domElement);
