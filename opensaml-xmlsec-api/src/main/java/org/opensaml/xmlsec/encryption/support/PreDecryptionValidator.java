3e17da09ed11cb7f69eb2b4330e3147d106a2048
Brent Putman <putmanb@georgetown.edu>
2023-01-09 22:26:22 -0500

    OSJ-367: Decrypter does not guard against malicious references ...
    Decrypter does not guard against malicious references or transforms.

---
+++ opensaml-xmlsec-api/src/main/java/org/opensaml/xmlsec/encryption/support/PreDecryptionValidator.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.xmlsec.encryption.support;
+
+import javax.annotation.Nonnull;
+
+import org.opensaml.xmlsec.encryption.EncryptedData;
+import org.opensaml.xmlsec.encryption.EncryptedKey;
+
+/**
+ * Component which performs validation of encrypted types prior to decryption.
+ */
+public interface PreDecryptionValidator {
+    
+    /**
+     * Validate an instance of {@link EncryptedData}.
+     * 
+     * @param encryptedData the target to validate
+     * 
+     * @throws PreDecryptionValidationException if the target fails validation
+     */
+    public void validate(@Nonnull final EncryptedData encryptedData) throws PreDecryptionValidationException;
+
+    /**
+     * Validate an instance of {@link EncryptedKey}.
+     * 
+     * @param encryptedKey the target to validate
+     * 
+     * @throws PreDecryptionValidationException if the target fails validation
+     */
+    public void validate(@Nonnull final EncryptedKey encryptedKey) throws PreDecryptionValidationException;
+
+}
---
+++ opensaml-xmlsec-impl/src/test/java/org/opensaml/xmlsec/encryption/support/tests/DefaultPreDecryptionValidatorTest.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the University Corporation for Advanced Internet Development,
+ * Inc. (UCAID) under one or more contributor license agreements.  See the
+ * NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The UCAID licenses this file to You under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensaml.xmlsec.encryption.support.tests;
+
+import org.opensaml.core.testing.XMLObjectBaseTestCase;
+import org.opensaml.core.xml.util.XMLObjectSupport;
+import org.opensaml.xmlsec.encryption.CipherData;
+import org.opensaml.xmlsec.encryption.CipherReference;
+import org.opensaml.xmlsec.encryption.EncryptedData;
+import org.opensaml.xmlsec.encryption.EncryptedKey;
+import org.opensaml.xmlsec.encryption.support.DefaultPreDecryptionValidator;
+import org.opensaml.xmlsec.encryption.support.PreDecryptionValidationException;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ */
+public class DefaultPreDecryptionValidatorTest extends XMLObjectBaseTestCase {
+    
+    private DefaultPreDecryptionValidator validator;
+    
+    private EncryptedData encryptedData;
+    private EncryptedKey encryptedKey;
+    
+    @BeforeMethod
+    public void setup() throws Exception {
+        validator = new DefaultPreDecryptionValidator();
+        
+        encryptedData = (EncryptedData) XMLObjectSupport.buildXMLObject(EncryptedData.DEFAULT_ELEMENT_NAME);
+        encryptedData.setCipherData((CipherData) XMLObjectSupport.buildXMLObject(CipherData.DEFAULT_ELEMENT_NAME));
+
+        encryptedKey = (EncryptedKey) XMLObjectSupport.buildXMLObject(EncryptedKey.DEFAULT_ELEMENT_NAME);
+        encryptedKey.setCipherData((CipherData) XMLObjectSupport.buildXMLObject(CipherData.DEFAULT_ELEMENT_NAME));
+    }
+    
+    @Test
+    public void goodEncryptedData() throws Exception {
+       validator.validate(encryptedData); 
+    }
+
+    @Test
+    public void goodEncryptedKey() throws Exception {
+       validator.validate(encryptedKey); 
+    }
+
+    @Test(expectedExceptions=PreDecryptionValidationException.class)
+    public void noCipherData() throws Exception {
+        encryptedData.setCipherData(null); 
+        validator.validate(encryptedData); 
+    }
+
+    @Test(expectedExceptions=PreDecryptionValidationException.class)
+    public void encryptedDataWithCipherReference() throws Exception {
+        encryptedData.getCipherData().setCipherReference((CipherReference) XMLObjectSupport.buildXMLObject(CipherReference.DEFAULT_ELEMENT_NAME));
+        validator.validate(encryptedData); 
+    }
+
+    @Test(expectedExceptions=PreDecryptionValidationException.class)
+    public void encryptedKeyWithCipherReference() throws Exception {
+        encryptedKey.getCipherData().setCipherReference((CipherReference) XMLObjectSupport.buildXMLObject(CipherReference.DEFAULT_ELEMENT_NAME));
+        validator.validate(encryptedKey); 
+    }
+
+}
