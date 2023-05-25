e0087f3749b80c8c96592bbe8a5a2afde4401100
Scott Cantor <cantor.2@osu.edu>
2023-05-25 12:45:08 -0400

    Add a slot for a counter to increment.

--- opensaml-profile-impl/src/main/java/org/opensaml/profile/action/impl/PopulateMetricContext.java
+++ opensaml-profile-impl/src/main/java/org/opensaml/profile/action/impl/PopulateMetricContext.java
@@ -22,14 +22,18 @@ import java.util.function.Function;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
+import org.opensaml.core.metrics.MetricsSupport;
 import org.opensaml.profile.action.AbstractProfileAction;
 import org.opensaml.profile.context.MetricContext;
 import org.opensaml.profile.context.ProfileRequestContext;
 import org.slf4j.Logger;
 
+import com.codahale.metrics.MetricRegistry;
+
 import net.shibboleth.shared.annotation.constraint.NonnullAfterInit;
 import net.shibboleth.shared.component.ComponentInitializationException;
 import net.shibboleth.shared.primitive.LoggerFactory;
+import net.shibboleth.shared.primitive.StringSupport;
 
 
 /**
@@ -40,7 +44,9 @@ import net.shibboleth.shared.primitive.LoggerFactory;
  * <p>Unlike a more typical "lookup strategy" design used in most other places, the strategy function
  * supplied is free, and indeed expected, to directly manipulate the created child context directly
  * rather than returning the data to use. The function may return false to indicate a lack of success,
- * but this value is merely logged.</p>
+ * but this is merely logged.</p>
+ * 
+ * <p>A side effect of this action is the incrementing of a counter corresponding to the profile running.</p>
  * 
  * @event {@link org.opensaml.profile.action.EventIds#PROCEED_EVENT_ID}
  */
@@ -49,8 +55,24 @@ public class PopulateMetricContext extends AbstractProfileAction {
     /** Class logger. */
     @Nonnull private final Logger log = LoggerFactory.getLogger(PopulateMetricContext.class);
     
+    /** Counter to increment on execution. */
+    @Nullable private String counterName;
+    
     /** Strategy function for establishing metric mappings to apply. */
     @NonnullAfterInit private Function<ProfileRequestContext,Boolean> metricStrategy;
+    
+    /**
+     * Set a counter name to increment.
+     * 
+     * @param name counter name
+     * 
+     * @since 5.0.0
+     */
+    public void setCounterName(@Nullable final String name) {
+        checkSetterPreconditions();
+        
+        counterName = StringSupport.trimOrNull(name);
+    }
     
     /**
      * Set strategy to establish the metric mappings to use.
@@ -77,6 +99,14 @@ public class PopulateMetricContext extends AbstractProfileAction {
     @Override
     protected void doExecute(@Nonnull final ProfileRequestContext profileRequestContext) {
 
+        // Increment a counter if set.
+        if (counterName != null) {
+            final MetricRegistry registry = MetricsSupport.getMetricRegistry();
+            if (registry != null) {
+                registry.counter(counterName).inc();
+            }
+        }
+        
         final MetricContext metricCtx = new MetricContext();
         profileRequestContext.addSubcontext(metricCtx, true);
         if (!metricStrategy.apply(profileRequestContext)) {
